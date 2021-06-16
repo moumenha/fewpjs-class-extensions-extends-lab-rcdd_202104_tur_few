db42a3d3557628d0a6c96594d8aada8eaf08962a
Moumen Hallak <moumen95@gmail.com>
2021-06-16 23:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+  get perimeter() {
+    let sum = 0;
+    for(const side of this.sides)
+      sum += side;
+    return sum;
+  }
+}
+
+class Triangle extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+  get isValid() {
+   let side1 = this.sides[0];
+   let side2 = this.sides[1];
+   let side3 = this.sides[2];
+   if(side1 === side2 && side1 === side3 && side2 == side3){
+    return true;
+   } else {
+     return false;
+   }
+  }
+}
+
+class Square extends Polygon {
+  constructor(sides) {
+    super(sides);
+  }
+  get isValid() {
+    
+  }
+  
+}
+
